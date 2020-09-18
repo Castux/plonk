62f56fedd151384710132821f223ca434eb0560c
Noé Falzon <falzon.noe@gmail.com>
2020-09-18 13:55:59 +0300

    Local saving of text

--- plonk-web.lua
+++ plonk-web.lua
@@ -8,6 +8,24 @@ local formula_out = '</span>'
 
 local update_events
 
+local default_text = [[
+Welcome to Plonk, a minimalistic dice roller!<br>
+<br>
+You can type any text you want in here. Anything within brackets is considered to be a dice formula. For instance: [2d20 + 1]. Click on a formula to roll it! After editing the text, press tab or click outside the box to process it.<br>
+<br>
+Your text will be saved locally in your browser, but to be safe you should probably prepare and save the text in another editor!<br>
+<br>
+The notations "dX" and "XdY" are recognized, as well as all basic arithmetic operations:<br>
+<br>
+Just a [d6].<br>
+With a modifier [d10 + 3].<br>
+Multiple dice are added together: [4d6 + 10].<br>
+You can go as crazy as you like! [(3d6 * 5) + (4 + d4) * 3].<br>
+Use "//" for integer division, rounded down: [3d10 // 2].<br>
+<br>
+That's about it!<br>
+]]
+
 local function expand(num_faces, num_dice)
 
 	local res = {}
@@ -69,6 +87,34 @@ local function on_formula_clicked(elem, event)
     output_roll(formula, treated, result)
 end
 
+local storageKey = "lastText"
+
+local function save_locally()
+
+    local storage = js.global.window.localStorage
+    if storage then
+        storage:setItem(storageKey, input_div.innerHTML);
+    end
+end
+
+local function load_locally()
+
+    local function check_empty(text)
+        return text:gsub("<(.-)>", ""):gsub("%s", "") == ""
+    end
+
+    local storage = js.global.window.localStorage
+    if storage then
+        local text = storage:getItem(storageKey);
+
+        if text and not check_empty(text) then
+            input_div.innerHTML = text
+        else
+            input_div.innerHTML = default_text
+        end
+    end
+end
+
 local function treat_formulas()
 
     local txt = input_div.innerHTML
@@ -78,6 +124,7 @@ local function treat_formulas()
     input_div.innerHTML = txt
 
     update_events()
+    save_locally()
 end
 
 update_events = function()
@@ -97,6 +144,7 @@ local function setup()
 
     math.randomseed(os.time())
 
+    load_locally()
     treat_formulas()
 end
 
